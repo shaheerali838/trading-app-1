12feaecb4f9cf9e9d94b8041d658192f3815dd73
M. Usman Raza <mu951882@gmail.com>
2025-02-06 18:33:36 +0500

    nav bar responsivness improved

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,19 +24,19 @@ const Navbar = () => {
     <header className={`sticky top-0 z-10  smooth-transition ${isScrolled ? 'bg-opacity-50 backdrop-blur-lg' : 'bg-transparent'}`}>
       <nav className="container mx-auto flex justify-between items-center p-4">
         <div className="logo-container flex items-center">
-          <Link to={"/"} className=""><i><img className="w-[4vw]" src={logoSrc} alt="LOGO" /></i></Link>
+          <Link to={"/"} className=""><i><img className="w-[50px]" src={logoSrc} alt="LOGO" /></i></Link>
         </div>
-        <div className="flex space-x-4">
+        <div className="hidden sm:flex space-x-4 ">
           <Link to={"/trade"} className="text-white hover:text-[#00FF7F]">Trade</Link>
           <Link to={"/finance"} className="text-white hover:text-[#00FF7F]">Finance</Link>
           <Link to={"/about"} className="text-white hover:text-[#00FF7F]">About</Link>
           <Link to={"/another"} className="text-white hover:text-[#00FF7F]">Feature</Link>
         </div>
         <div className="flex items-center space-x-4">
-          <div className="w-[10vw] sm:w-[7vw] px-3 py-1 flex justify-center rounded-full bg-transparent border-2 border-[#1E90FF]">
+          <div className="min-w-[10vw] sm:w-[7vw] px-3 py-1 flex justify-center rounded-full bg-transparent border-2 border-[#1E90FF]">
             <button className="text-[#1E90FF]">Login</button>
           </div>
-          <div className="w-[10vw] sm:w-[7vw] px-2 py-1 flex justify-center rounded-full bg-[#1E90FF] text-white">
+          <div className="min-w-[10vw] sm:w-[7vw] px-2 py-1 flex justify-center rounded-full bg-[#1E90FF] text-white">
             <button>Register</button>
           </div>
         </div>
