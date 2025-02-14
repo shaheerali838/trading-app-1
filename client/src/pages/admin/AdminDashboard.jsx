b21d7e5b7a65e38bd6ea9ce6c2d77a2674b380f8
M. Usman Raza <mu951882@gmail.com>
2025-02-14 23:58:13 +0500

    server.js updated for frontend url

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import { Card} from "@material-tailwind/react";
-import API from "API";
+import { Card } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 import API from "../../utils/api";
 
@@ -26,9 +25,7 @@ const AdminDashboard = () => {
 
   const fetchTransactions = async () => {
     try {
-      const response = await API.get(
-        "/admin/transactions"
-      );
+      const response = await API.get("/admin/transactions");
       setTransactions(response.data);
     } catch (error) {
       console.error("Error fetching transactions:", error);
@@ -46,7 +43,6 @@ const AdminDashboard = () => {
 
   return (
     <div className="min-h-screen max-w-7xl mx-auto px-8 text-white">
-      
       <h1 className="text-4xl font-bold mb-6">Admin Dashboard</h1>
 
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -65,7 +61,10 @@ const AdminDashboard = () => {
           <h2 className="text-2xl font-bold mb-2 text-white">
             Transaction Approval
           </h2>
-          <p>Approve or Deny any Deposit or Withdraw Transaction seamlesy and effectively</p>
+          <p>
+            Approve or Deny any Deposit or Withdraw Transaction seamlesy and
+            effectively
+          </p>
           <Link to={"/admin/transaction/manage"}>
             <p className="text-blue-500 hover:underline cursor-pointer">
               Transaction Management
@@ -73,9 +72,7 @@ const AdminDashboard = () => {
           </Link>
         </Card>
         <Card className="rounded-lg bg-[#2A2A2A] text-gray-500 p-6 hover:scale-105 transition duration-300 ease-in-out">
-          <h2 className="text-2xl font-bold mb-2 text-white">
-            System Logs
-          </h2>
+          <h2 className="text-2xl font-bold mb-2 text-white">System Logs</h2>
           <p>View, Analyze, Block any user seamlesy and effectively</p>
           <Link to={"/admin/users/manage"}>
             <p className="text-blue-500 hover:underline cursor-pointer">
