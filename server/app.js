67f95deb00a2f3c2618fabc0c2c7fc858f2905d5
M. Usman Raza <mu951882@gmail.com>
2025-02-15 00:33:16 +0500

    cors configuration updated

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@
   
   app.use(
     cors({
-      origin: [process.env.FRONTEND_URL],
+      origin: ["https://sienna-cassowary-102747.hostingersite.com"],
       credentials: true,
       methods: "GET,POST,PUT,DELETE",
       allowedHeaders: "Content-Type,Authorization",
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://sienna-cassowary-102747.hostingersite.com/"],
+    origin: ["https://sienna-cassowary-102747.hostingersite.com"],
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
