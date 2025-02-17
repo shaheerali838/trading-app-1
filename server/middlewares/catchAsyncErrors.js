ea7c2d21793eef799fb0a53f3b99bc7fc34d0e5d
M. Usman Raza <mu951882@gmail.com>
2025-02-17 09:25:21 +0500

    error handling modified

--- server/middlewares/catchAsyncErrors.js
+++ server/middlewares/catchAsyncErrors.js
@@ -1,10 +1,5 @@
-import ErrorHandler from './errorMiddleware.js'
 export const catchAsyncErrors = (func) => (req, res, next) => {
-Promise.resolve(func(req, res, next)).catch((error) => {
-    if (error.name === "ValidationError") {
-      next(new ErrorHandler(Object.values(error.errors).map(e => e.message).join(", "), 400));
-    } else {
-      next(error);
-    }
+  Promise.resolve(func(req, res, next)).catch((error) => {
+    next(error);
   });
 };
--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -2,44 +2,41 @@ class ErrorHandler extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = true; 
+    this.isOperational = true;
   }
 }
 
 export const errorMiddleware = (err, req, res, next) => {
   console.log('Reached error middleware');
+  
   if (!(err instanceof ErrorHandler)) {
     console.error("Unhandled Error:", err);
     err = new ErrorHandler("Internal Server Error", 500);
   }
+  
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ErrorHandler(message, 400);
+    err = new ErrorHandler(`Duplicate ${Object.keys(err.keyValue)} Entered`, 400);
   }
   if (err.name === "JsonWebTokenError") {
-    const message = `Json Web Token is invalid, Try again!`;
-    err = new ErrorHandler(message, 400);
+    err = new ErrorHandler("Json Web Token is invalid, Try again!", 400);
   }
   if (err.name === "TokenExpiredError") {
-    const message = `Json Web Token is expired, Try again!`;
-    err = new ErrorHandler(message, 400);
+    err = new ErrorHandler("Json Web Token is expired, Try again!", 400);
   }
   if (err.name === "CastError") {
-    const message = `Invalid ${err.path}`,
-      err = new ErrorHandler(message, 400);
+    err = new ErrorHandler(`Invalid ${err.path}`, 400);
   }
 
-  const errorMessage = err.errors
-    ? Object.values(err.errors)
-        .map((error) => error.message)
-        .join(" ")
-    : err.message;    
+  const errorMessage =
+    err.errors && typeof err.errors === "object"
+      ? Object.values(err.errors).map((error) => error.message).join(" ")
+      : err.message;
 
   return res.status(err.statusCode).json({
-    success: false,   
+    success: false,
     message: errorMessage,
   });
 };
